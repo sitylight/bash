fcf3be3bc592b8c30256e647ca7ff0c13509fd18
sitylight <dl.cc.3.1415926@gmail.com>
2022-12-05 14:57:28 +0800

    update

--- gen-java-jks-default.sh
+++ gen-java-jks-default.sh
@@ -5,10 +5,13 @@ keysize='2048'
 keystore='server.jks'
 validity='365'
 password='123456'
-dname='CN=DL, OU=CL, O=DD, L=ZH, ST=GD, C=CN'
+dname="CN=DL,O=temp,L=ZH,ST=GD,C=CN"
 
 echo "init alias: ${alias}, keysize: ${keysize}, keystore: ${keystore}, validity: ${validity}, password: ${password}"
 
 keytool -genkeypair -alias ${alias} -keyalg RSA -dname ${dname} -keysize ${keysize} -keystore ${keystore} -validity ${validity} -storetype jks -storepass ${password} -keypass ${password}
 
+# keytool -importkeystore -srckeystore server.jks -destkeystore server.jks -deststoretype pkcs12 -storepass ${password}
+
 echo "${keystore} is generated"
+
